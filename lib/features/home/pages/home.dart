15665e1f3a5c184798e1f5879420ee36fdb573b8
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2025-05-04 13:26:43 +0300

    feat(home): add refresh indicator

--- lib/features/home/pages/home.dart
+++ lib/features/home/pages/home.dart
@@ -19,14 +19,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late List<Activity> activities;
   int? selectedActivityId;
+  late Future<List<Activity>> getActivitiesFuture;
 
   @override
   void initState() {
     super.initState();
     selectedActivityId = null;
+    getActivitiesFuture = getActivities();
   }
 
-  void _onActivityChanged(int activityId) {
+  Future<void> onRefresh() async {
+    setState(() {
+      getActivitiesFuture = getActivities();
+    });
+    await getActivitiesFuture;
+  }
+
+  void onActivityChanged(int activityId) {
     setState(() {
       selectedActivityId = activityId;
     });
@@ -37,77 +46,84 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: appBar(),
       backgroundColor: gmPrimaryBackgroundColor,
-      body: FutureBuilder<List<Activity>>(
-        future: getActivities(),
-        builder: (context, activitiesSnapshot) {
-          if (activitiesSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-          if (activitiesSnapshot.hasError || activitiesSnapshot.data == null) {
-            return Center(child: Text('Failed to load activities'));
-          }
+      body: RefreshIndicator(
+        onRefresh: onRefresh,
+        child: FutureBuilder<List<Activity>>(
+          future: getActivitiesFuture,
+          builder: (context, activitiesSnapshot) {
+            if (activitiesSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (activitiesSnapshot.hasError ||
+                activitiesSnapshot.data == null) {
+              return Center(child: Text('Failed to load activities'));
+            }
 
-          activities = activitiesSnapshot.data!;
-          selectedActivityId ??= activities.first.id;
+            activities = activitiesSnapshot.data!;
+            selectedActivityId ??= activities.first.id;
 
-          return Padding(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 20,
-              children: [
-                ActivityDropdown(
-                  selectedActivityId: selectedActivityId!,
-                  activities: activities,
-                  onChanged: _onActivityChanged,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  spacing: 30,
+            return SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 20,
                   children: [
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        spacing: 20,
-                        children: [
-                          TitleBox(
-                            title: "Игроки",
-                          ),
-                          Container(
-                              width: double.infinity,
-                              height: 300,
-                              decoration: gmBoxDecoration(),
-                              padding: EdgeInsets.all(20),
-                              child: PlayerList(
-                                  selectedActivityId: selectedActivityId!)),
-                        ],
-                      ),
+                    ActivityDropdown(
+                      selectedActivityId: selectedActivityId!,
+                      activities: activities,
+                      onChanged: onActivityChanged,
                     ),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        spacing: 20,
-                        children: [
-                          TitleBox(title: "Игры"),
-                          Container(
-                              width: double.infinity,
-                              height: 300,
-                              decoration: gmBoxDecoration(),
-                              padding: EdgeInsets.all(20),
-                              child: GameList(
-                                  selectedActivityId: selectedActivityId!)),
-                        ],
-                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      spacing: 30,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            spacing: 20,
+                            children: [
+                              TitleBox(
+                                title: "Игроки",
+                              ),
+                              Container(
+                                  width: double.infinity,
+                                  height: 300,
+                                  decoration: gmBoxDecoration(),
+                                  padding: EdgeInsets.all(20),
+                                  child: PlayerList(
+                                      selectedActivityId: selectedActivityId!)),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            spacing: 20,
+                            children: [
+                              TitleBox(title: "Игры"),
+                              Container(
+                                  width: double.infinity,
+                                  height: 300,
+                                  decoration: gmBoxDecoration(),
+                                  padding: EdgeInsets.all(20),
+                                  child: GameList(
+                                      selectedActivityId: selectedActivityId!)),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
-          );
-        },
+              ),
+            );
+          },
+        ),
       ),
     );
   }

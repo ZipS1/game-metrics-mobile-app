e8360c9bc9ff0702b2f4e4a644356872519542f1
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2025-05-05 18:38:25 +0300

    chore: set visible DateTime to local

--- lib/features/home/widgets/game_info_row.dart
+++ lib/features/home/widgets/game_info_row.dart
@@ -3,6 +3,7 @@ import 'package:game_metrics_mobile_app/common/styles/text_styles.dart';
 import 'package:intl/intl.dart';
 
 Widget gameInfoRow(DateTime date, Duration duration) {
+  date = date.toLocal();
   final formattedDate = DateFormat('dd.MM.yyyy').format(date);
   final formattedTime = DateFormat('HH:mm').format(date);
   final formattedDuration =
